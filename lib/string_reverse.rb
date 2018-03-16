292cd9fae2974f6c2c469d957883b1f4be8198d9
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-03-16 09:50:42 -0700

    string reverse and tests

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,17 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  # my_string << "not implemented"
+  return nil if my_string.nil?
+  return my_string if my_string.length ==0 || my_string.length == 1
+
+  count = 0
+  l = my_string.length - 1
+  while count < l
+    letter = my_string[count]
+    my_string[count] = my_string[l]
+    my_string[l] = letter
+    count += 1
+    l -= 1
+  end
+  return 
 end
