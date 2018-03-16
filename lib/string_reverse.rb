650fc3543d8842c32ef21793b76fbd749dd1f5b8
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-03-16 12:22:30 -0700

    this is working.

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,22 @@
+require 'pry'
+
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  if my_string == nil
+    return nil
+  elsif my_string.length == 1
+    return my_string
+  else
+    origin = my_string.length - 1
+    target = 0
+    temp = nil
+    (my_string.length / 2).times do
+      temp = my_string[target]
+      my_string[target] = my_string[origin]
+      my_string[origin] = temp
+      origin -= 1
+      target += 1
+    end
+  end
+  return my_string
 end
