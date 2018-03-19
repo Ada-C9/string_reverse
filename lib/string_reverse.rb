13a4ef6b4299b4516a660845deb03e851ac4a375
Abiaina <abinnet.a@gmail.com>
2018-03-19 01:19:28 -0700

    all tests fail, need to confirm index works on string

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,12 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  index = 0
+  while index <= (my_string.length - index)
+    first = my_string[index]
+
+    my_string[index] = my_string[(my_string.length -index)]
+
+    my_string[index] = first
+  end
+  return my_string
 end
