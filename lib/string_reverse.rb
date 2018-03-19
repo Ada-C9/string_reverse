a7a5ae3cddcf4d7274e1797824bd3976f7646c16
Samantha Berk <sberk10@gmail.com>
2018-03-19 16:37:40 -0700

    Added the code and ran the tests

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,22 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  if my_string == nil
+    return my_string
+  end
+
+  i = 0
+  n = my_string.length - 1
+
+  while i < n
+    letter = my_string[i]
+    my_string[i] = my_string[n]
+    my_string[n] = letter
+    i += 1
+    n -= 1
+  end
+  return my_string
 end
+
+puts "enter a string: "
+str = gets.chomp
+p string_reverse(str)
