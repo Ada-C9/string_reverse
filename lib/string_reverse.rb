a856d9244bda7e169c746b0906ababdadf8e2bce
Tor Shimizu <torshimizu@gmail.com>
2018-03-19 16:43:33 -0700

    refactored code to optimize time-complexity

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,18 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  return if my_string == nil
+  length = my_string.length
+  return if length == 0 || length == 1
+  i = 0
+  j = length - 1
+
+  while i < j
+    temp = my_string[i]
+    my_string[i] = my_string[j]
+    my_string[j] = temp
+    i += 1
+    j -= 1
+  end
+
+  return my_string
 end
