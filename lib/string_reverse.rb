520b86a091cdfeb7f4fb7d381e91de90484766c6
Kat <kat.eastman@gmail.com>
2018-03-19 22:50:22 -0700

    tests are green.

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,18 @@
+require 'pry'
 # A method to reverse a string in place.
+
 def string_reverse(my_string)
-  my_string << "not implemented"
+return my_string if my_string == nil || my_string.length <= 1
+
+  start_index = 0
+  end_index = my_string.length - 1
+  while start_index < end_index
+    temp_storage = my_string[start_index]
+    my_string[start_index] = my_string[end_index]
+    my_string[end_index] = temp_storage
+    start_index += 1
+    end_index -= 1
+  end
+
+  return my_string
 end
