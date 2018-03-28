32a7ac9eb040db01236b44a682b743c657df0555
Kirsten Schumy <kirsten.schumy@gmail.com>
2018-03-27 21:20:50 -0700

    revised solutions

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,12 +1,20 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
   return if my_string.nil?
-  i = 0
-  while i < my_string.length / 2
-    temp_char = my_string[i]
-    my_string[i] = my_string[my_string.length - 1 - i]
-    my_string[my_string.length - 1 - i] = temp_char
-    i += 1
-  end
+  reverse_helper(my_string.length, 0, my_string)
+  # (my_string.length / 2).times do |index|
+  #   temp_char = my_string[index]
+  #   my_string[index] = my_string[my_string.length - 1 - index]
+  #   my_string[my_string.length - 1 - index] = temp_char
+  # end
+end
 
+
+
+def reverse_helper(start_index, curr_index, my_words)
+  ((start_index - curr_index) / 2).times do |count|
+    temp = my_words[curr_index + count]
+    my_words[curr_index + count] = my_words[start_index - count - 1]
+    my_words[start_index - count - 1] = temp
+  end
 end
