6edec791537db8dd65fb40ed40eb39d147ba1fba
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-04-04 18:47:05 -0700

    Write functionality for string_reverse method

--- lib/string_reverse.rb
+++ lib/string_reverse.rb
@@ -1,4 +1,17 @@
 # A method to reverse a string in place.
 def string_reverse(my_string)
-  my_string << "not implemented"
+  return nil if my_string.nil?
+  bottom = 0
+  top = my_string.length - 1
+  while bottom <= top
+    temporary_character = my_string[top]
+    my_string[top] = my_string[bottom]
+    my_string[bottom] = temporary_character
+
+    bottom += 1
+    top -= 1
+  end
+  return my_string
 end
+
+puts string_reverse("Lovelace")
